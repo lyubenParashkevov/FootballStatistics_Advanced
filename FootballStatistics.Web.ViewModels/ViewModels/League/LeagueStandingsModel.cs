34bf770e0949221995184c8275418c1730de362e
lyubenParashkevov <lubodp_1981@abv.bg>
2026-03-05 14:44:32 +0200

    Add LeagueStandingsModel for league table

---
+++ FootballStatistics.Web.ViewModels/ViewModels/League/LeagueStandingsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStatistics.Web.ViewModels.ViewModels.League
+{
+    public class LeagueStandingsModel
+    {
+        public int LeagueId { get; set; }
+        public string LeagueName { get; set; } = null!;
+
+        public IEnumerable<LeagueStandingsRowModel> Table { get; set; }
+            = new List<LeagueStandingsRowModel>();
+    }
+}

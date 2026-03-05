ca0a92ccbdbbbf5fe33c866324b88cc2814e7ec0
lyubenParashkevov <lubodp_1981@abv.bg>
2026-03-05 14:19:07 +0200

    Add LeagueStandingsRowModel view model

---
+++ FootballStatistics.Web.ViewModels/ViewModels/League/LeagueStandingsRowModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStatistics.Web.ViewModels.ViewModels.League
+{
+    public class LeagueStandingsRowModel
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; } = null!;
+        public int Played { get; set; }
+        public int Wins { get; set; }
+        public int Draws { get; set; }
+        public int Losses { get; set; }
+
+        public int GoalsFor { get; set; }
+        public int GoalsAgainst { get; set; }
+        public int GoalDifference => GoalsFor - GoalsAgainst;
+        public int Points { get; set; }
+    }
+}

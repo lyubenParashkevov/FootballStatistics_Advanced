32456f04282a305669dc2004e4d392498db2a082
lyubenParashkevov <lubodp_1981@abv.bg>
2026-03-09 16:09:58 +0200

    Add Player view models and TeamDropdownModel for player forms

---
+++ FootballStatistics.Web.ViewModels/ViewModels/Player/PlayerDetailsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStatistics.Web.ViewModels.ViewModels.Player
+{
+    public class PlayerDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int Age { get; set; }
+
+        public string Position { get; set; } = null!;
+
+        public int GoalsScored { get; set; }
+
+        public string TeamName { get; set; } = null!;
+    }
+}
---
+++ FootballStatistics.Web.ViewModels/ViewModels/Player/PlayerFormModel.cs
@@ -0,0 +1,29 @@
+﻿using FootballStatistics.ViewModels.League;
+using FootballStatistics.Web.ViewModels.ViewModels.Teams;
+using System.ComponentModel.DataAnnotations;
+using static FootballStatistics.Common.ValidationConstants;
+
+
+namespace FootballStatistics.Web.ViewModels.ViewModels.Player
+{
+    public class PlayerFormModel
+    {
+        [Required]
+        [StringLength(PlayerNameMaxLength, MinimumLength = PlayerNameMinLength)]
+        public string Name { get; set; } = null!;
+
+        [Range(PlayerMinAge, PlayerMaxAge)]
+        public int Age { get; set; }
+
+        [Required]
+        [StringLength(PlayerPositionMaxLength, MinimumLength = PlayerPositionMinLength)]
+        public string Position { get; set; } = null!;
+
+        [Range(PlayerMinGoalsScored, PlayerMaxGoalsScored)]
+        public int GoalsScored { get; set; }
+
+        public int TeamId { get; set; }
+
+        public IEnumerable<TeamDropdownModel>? Teams { get; set; }
+    }
+}

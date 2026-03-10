727b4f0fd9efe69174bfda1cf23e1f7918749e2c
lyubenParashkevov <lubodp_1981@abv.bg>
2026-03-10 12:15:27 +0200

    Add IPlayerService contract

---
+++ FootballStatistics.Services/Contracts/IPlayerService.cs
@@ -0,0 +1,24 @@
+﻿using FootballStatistics.Web.ViewModels.ViewModels.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStatistics.Services.Contracts
+{
+    public interface IPlayerService
+    {
+        Task<IEnumerable<PlayerListItemModel>> GetAllAsync();
+
+        Task CreateAsync(PlayerFormModel model);
+
+        Task<PlayerDetailsViewModel?> GetDetailsAsync(int id);
+
+        Task<PlayerFormModel?> GetEditModelAsync(int id);
+
+        Task<bool> UpdateAsync(int id, PlayerFormModel model);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}

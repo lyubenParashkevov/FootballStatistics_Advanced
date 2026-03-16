ab31d32695d6656e376bab262d2b42c73f227b84
lyubenParashkevov <lubodp_1981@abv.bg>
2026-03-16 12:21:09 +0200

    Add Stadium view models

---
+++ FootballStatistics.Web.ViewModels/ViewModels/Stadium/StadiumDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStatistics.Web.ViewModels.ViewModels.Stadium
+{
+    public class StadiumDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string City { get; set; } = null!;
+
+        public int Capacity { get; set; }
+    }
+}
---
+++ FootballStatistics.Web.ViewModels/ViewModels/Stadium/StadiumFormModel.cs
@@ -0,0 +1,27 @@
+﻿using FootballStatistics.Web.ViewModels.ViewModels.Teams;
+using System;
+
+using System.ComponentModel.DataAnnotations;
+using static FootballStatistics.Common.ValidationConstants;
+
+namespace FootballStatistics.Web.ViewModels.ViewModels.Stadium
+{
+    public class StadiumFormModel
+    {
+        [Required]
+        [StringLength(StadiumNameMaxLength, MinimumLength = StadiumNameMinLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(StadiumCityMaxLength, MinimumLength = StadiumCityMinLength)]
+        public string City { get; set; } = null!;
+
+        [Range(StadiumCapacityMinValue, StadiumCapacityMaxValue)]
+        public int Capacity { get; set; }
+
+        public int TeamId { get; set; }
+
+        public IEnumerable<TeamDropdownModel> Teams { get; set; }
+            = new List<TeamDropdownModel>();
+    }
+}

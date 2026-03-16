b9e10fbef6539cbded9604bae62b1d28b8183ff0
lyubenParashkevov <lubodp_1981@abv.bg>
2026-03-16 14:05:03 +0200

    Add IStadiumService contract

---
+++ FootballStatistics.Services/Contracts/IStadiumService.cs
@@ -0,0 +1,26 @@
+﻿using FootballStatistics.Web.ViewModels.ViewModels.Stadium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStatistics.Services.Contracts
+{
+    public interface IStadiumService
+    {
+        Task<IEnumerable<StadiumListItemModel>> GetAllAsync();
+
+        Task<StadiumFormModel> GetCreateModelAsync();
+
+        Task CreateAsync(StadiumFormModel model);
+
+        Task<StadiumDetailsViewModel?> GetDetailsAsync(int id);
+
+        Task<StadiumFormModel?> GetEditModelAsync(int id);
+
+        Task<bool> UpdateAsync(int id, StadiumFormModel model);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}

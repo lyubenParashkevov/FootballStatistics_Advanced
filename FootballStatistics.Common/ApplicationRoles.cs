e26eb8bd9a5fe9f3755f1bd159423d6809d3df33
lyubenParashkevov <lubodp_1981@abv.bg>
2026-03-23 15:27:35 +0200

    Add application roles constants, сeed application roles and administrator account

---
+++ FootballStatistics.Common/ApplicationRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStatistics.Common
+{
+    public static class ApplicationRoles
+    {
+        public const string Administrator = "Administrator";
+        public const string User = "User";
+    }
+}
--- FootballStatistics.Web/Program.cs
+++ FootballStatistics.Web/Program.cs
@@ -3,6 +3,8 @@ using FootballStatistics.Services;
 using FootballStatistics.Data.Infrastructure.Database;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using FootballStatistics.Common;
+
 
 namespace FootballStatistics.Web
 {
@@ -49,6 +51,17 @@ namespace FootballStatistics.Web
                 app.UseHsts();
             }
 
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+
+                SeedRolesAndAdminAsync(roleManager, userManager).GetAwaiter().GetResult();
+            }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -78,6 +91,50 @@ namespace FootballStatistics.Web
             options.Password.RequireNonAlphanumeric = true;
             options.Password.RequireUppercase = true;
             options.Password.RequiredLength = 8;
+        }
+
+        static async Task SeedRolesAndAdminAsync(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            string[] roles = { ApplicationRoles.Administrator, ApplicationRoles.User };
+
+            foreach (var role in roles)
+            {
+                bool roleExists = await roleManager.RoleExistsAsync(role);
+
+                if (!roleExists)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            string adminEmail = "admin@footballstatistics.com";
+            string adminPassword = "Admin123!";
+
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+            if (adminUser == null)
+            {
+                adminUser = new IdentityUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true
+                };
+
+                var result = await userManager.CreateAsync(adminUser, adminPassword);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, ApplicationRoles.Administrator);
+                }
+            }
+            else
+            {
+                if (!await userManager.IsInRoleAsync(adminUser, ApplicationRoles.Administrator))
+                {
+                    await userManager.AddToRoleAsync(adminUser, ApplicationRoles.Administrator);
+                }
+            }
         }
     }
 }

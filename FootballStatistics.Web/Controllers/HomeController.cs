6ee64a81fe6e8166e04861dd997672046864ce2a
lyubenParashkevov <lubodp_1981@abv.bg>
2026-03-31 21:39:36 +0300

    Add 404 and 500 error pages

--- FootballStatistics.Web/Controllers/HomeController.cs
+++ FootballStatistics.Web/Controllers/HomeController.cs
@@ -6,23 +6,38 @@ namespace FootballStatistics.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
         public IActionResult Index()
         {
             return View();
+            
         }
-      
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpGet]
+        public IActionResult Error404()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Error500()
+        {
+            return View();
+        }
+
+        
     }
 }
--- FootballStatistics.Web/Program.cs
+++ FootballStatistics.Web/Program.cs
@@ -43,17 +43,19 @@ namespace FootballStatistics.Web
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
+                app.UseExceptionHandler("/Home/Error500");
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Home/Error500");
                 app.UseHsts();
             }
+
+            app.UseStatusCodePagesWithReExecute("/Home/Error404");
 
 
             using (var scope = app.Services.CreateScope())

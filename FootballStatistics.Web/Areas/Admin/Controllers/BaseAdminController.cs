b7a669c5ae5b506629e5d46b3a3b0901e6030ba3
lyubenParashkevov <lubodp_1981@abv.bg>
2026-04-01 10:25:38 +0300

    Add base admin controller, admin home controllerand view for administrator area

---
+++ FootballStatistics.Web/Areas/Admin/Controllers/BaseAdminController.cs
@@ -0,0 +1,14 @@
+﻿using FootballStatistics.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballStatistics.Web.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    [Authorize(Roles = ApplicationRoles.Administrator)]
+    public class BaseAdminController : Controller
+    {
+   
+    }
+}
---
+++ FootballStatistics.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FootballStatistics.Web.Areas.Admin.Controllers
+{
+    public class HomeController : BaseAdminController
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
